8705c59d695b064a010103a643afc12daa9a2f97
yadavDilip7 <dilipyadav4464@gmail.com>
2024-04-06 07:49:14 +0530

    new commit

--- 01vitereact/02counter/03tailwindprops/src/App.jsx
+++ 01vitereact/02counter/03tailwindprops/src/App.jsx
@@ -2,32 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myobj={
+    usernam:"Dilip",
+    age:22,
+    
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    <h1 className = 'bg-purple-400 text-black p-4 rounded-xl'>Apple Store</h1>
+    
+        <Card username= "Apple Macbook M1 pro" btnText="Buy at $6000"/>  
+        <Card username="Apple Macbook M2 pro" btnText="Buy at $7000" /> 
+        <Card username="Apple Macbook M2"/>       
+
+  
     </>
   )
 }
